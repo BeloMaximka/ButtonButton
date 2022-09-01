e86b1506619d8eb578f228262a905d24446d0638
BeloMaximka <belomaxim@gmail.com>
2022-09-01 13:32:38 +0300

    knopoki update

--- Again/Form1.Designer.cs
+++ Again/Form1.Designer.cs
@@ -28,33 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(480, 152);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Кнопоки";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Button button1;
     }
 }
--- Again/Form1.cs
+++ Again/Form1.cs
@@ -2,19 +2,35 @@ namespace Again
 {
     public partial class Form1 : Form
     {
+        Button currentButton;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_MouseMove(object sender, MouseEventArgs e)
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            Button button = sender as Button;
+            currentButton = new Button();
+            currentButton.Location = e.Location;
+            currentButton.Size = new Size(0, 0);
+            currentButton.Text = "Кнопока";
+
             Random random = new Random();
-            
-            int x = random.Next(0, this.ClientSize.Width - button.Size.Width);
-            int y = random.Next(0, this.ClientSize.Height - button.Size.Height);
-            button.Location = new Point(x, y);
+            currentButton.BackColor = Color.FromArgb(random.Next(2147483647));
+
+            this.Controls.Add(currentButton);
+            currentButton.BringToFront();
+            this.MouseMove += Form1_MouseMove;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            currentButton.Size = new Size(e.Location.X - currentButton.Location.X, e.Location.Y - currentButton.Location.Y);
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.MouseMove -= Form1_MouseMove;
         }
     }
 }
